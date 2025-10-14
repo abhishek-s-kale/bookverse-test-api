d1dc03293dbfd87eeb61d8c5df4458e1baa05711
abhishek-s-kale <abhsihekkale@cybagee.com>
2025-10-14 23:35:15 +0530

    attempt to fix unit testing

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,17 +1,15 @@
 /** @type
  {import('ts-jest).JestConfigwithTSJest } */
+
  // jest.config.js
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'], // Specify the root directory for tests
-  modulefileExtensions: ['ts', 'js', 'json'], // File extensions to consider
+  moduleFileExtensions: ['ts', 'js', 'json'], // File extensions to consider
   // Optional: Define where Jest should look for test files
-   globals: {
-        'ts-jest': {
-          tsconfig: 'tsconfig.json',
-        },
-      },
+ transform: {'^.+\\.ts$': ['ts-jest',{useESM:true}]},
+ extensionsToTreatAsEsm: ['.ts'],
   testMatch: ['**/tests/**/*.test.ts'], 
   verbose: true,
 };
